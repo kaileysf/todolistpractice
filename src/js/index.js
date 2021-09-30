10622b55ec6b6454d4a00e5712455c1715b37aae
kaileysf <kfriedrich1@nait.ca>
2021-09-29 18:46:37 -0600

    app shell is ready

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,4 @@
-console.log(app);
+
+const app = documnet.querySelector('#app');
+
+app.innerHTML = `<h1>HOME PAGE </h1>`
---
+++ src/js/utils/dataFetcher.js
@@ -0,0 +1,21 @@
+/*
+
+    dataFetcher (url)
+
+    description:
+
+    parameters:
+    url:string
+
+    async -------> promise
+
+*/
+
+const dataFetcher = async function (url = null) {
+    const response = await fetch(url);
+    const data = await response.json();
+
+    return data
+};
+
+export default dataFetcher;
